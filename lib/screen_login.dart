beb667d905550afc822593359cbf4787b2c95f52
EmpiereTech-Avadh <developer.empiere@gmail.com>
2025-03-27 13:01:24 +0530

    - Created a basic form UI for login and navigated to the Home screen.

---
+++ lib/screen_login.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:form_test/screen_home.dart';
+
+class ScreenLogin extends StatefulWidget {
+  const ScreenLogin({super.key});
+
+  @override
+  State<ScreenLogin> createState() => _ScreenLoginState();
+}
+
+class _ScreenLoginState extends State<ScreenLogin> {
+  final TextEditingController tcUserName = TextEditingController();
+  final TextEditingController tcPassword = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    tcUserName.dispose();
+    tcPassword.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Login'), foregroundColor: Colors.white, backgroundColor: Colors.blue),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            spacing: 20.0,
+            children: [
+              TextFormField(controller: tcUserName, decoration: const InputDecoration(labelText: 'User Name')),
+              TextFormField(controller: tcPassword, decoration: const InputDecoration(labelText: 'Password')),
+              ElevatedButton(onPressed: login, child: Text('Login')),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  void login() {
+    if (tcUserName.text == 'username' && tcPassword.text == 'password') {
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => const ScreenHome()));
+    } else {
+      showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text('ERROR'),
+            content: Text('Invalid Username or Password.'),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text('OK'),
+              ),
+            ],
+          );
+        },
+      );
+    }
+  }
+}
